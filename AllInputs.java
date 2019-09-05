05f7639e9098c0555701697a22ba63e9b271ebc7
CHAGAN14122000 <52992296+CHAGAN14122000@users.noreply.github.com>
2019-09-05 18:12:38 +0530

    Add files via upload

---
+++ AllInputs.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class AllInputs{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the string value :");
+String a=sc.next();
+System.out.println("enter the long value :");
+long bcd=sc.nextLong();
+System.out.println("enter the double value :");
+double e=sc.nextDouble();
+System.out.println("enter the float value :");
+float f=sc.nextFloat();
+System.out.println("enter the character :");
+char g=sc.next().charAt(0);
+System.out.println("enter the byte value :");
+byte h=sc.nextByte();
+System.out.println("the string value is :"+a+"\nthe long value is :"+bcd+"\nthe double value is :"+e+"\nthe float value is :"+f+"\nthe character value is :"+g+"the byte value is :"+h);
+}
+}
+ 
